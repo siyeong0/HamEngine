3a3d274989923ad7944b759b3ea384bf67c850f5
DESKTOP-35395NG\james <jameskdw02@gmail.com>
2025-03-17 23:09:09 +0900

    MemPool module의 Page.ixx 구현

--- HamEngine/main.cpp
+++ HamEngine/main.cpp
@@ -1,64 +1,67 @@
-import std.core;
-
 import Common;
 import Renderer;
 import STL;
-
+import MemPool;
 using namespace ham;
+
+import std.core;
+
+struct TestStruct
+{
+	int Data1;
+	int Data2;
+	int Data3;
+	char Data4;
+};
 
 int main(void)
 {
-	class Foo
+
+
+	Page<TestStruct> testPage(1024);
+
+	testPage.DbgPrint();
+
+	TestStruct* block0 = testPage.AllocBlock();
+	TestStruct* block1 = testPage.AllocBlock();
+	TestStruct* block2 = testPage.AllocBlock();
+	TestStruct* block3 = testPage.AllocBlock();
+	TestStruct* block4 = testPage.AllocBlock();
+	TestStruct* block5 = testPage.AllocBlock();
+	TestStruct* block6 = testPage.AllocBlock();
+	TestStruct* block7 = testPage.AllocBlock();
+	TestStruct* block8 = testPage.AllocBlock();
+	TestStruct* block9 = testPage.AllocBlock();
+	TestStruct* block10 = testPage.AllocBlock();
+	TestStruct* block11 = testPage.AllocBlock();
+	TestStruct* block12 = testPage.AllocBlock();
+	TestStruct* block13 = testPage.AllocBlock();
+	TestStruct* block14 = testPage.AllocBlock();
+	TestStruct* block15 = testPage.AllocBlock();
+	TestStruct* block16 = testPage.AllocBlock();
+	TestStruct* block17 = testPage.AllocBlock();
+	TestStruct* block18 = testPage.AllocBlock();
+	TestStruct* block19 = testPage.AllocBlock();
+
+	testPage.DbgPrint();
+
+	testPage.FreeBlock(block6);
+	testPage.DbgPrint();
+	testPage.FreeBlock(block15);
+	testPage.DbgPrint();
+	testPage.FreeBlock(block17);
+	testPage.DbgPrint();
+
+	TestStruct* block20 = testPage.AllocBlock();
+	testPage.DbgPrint();
+
+
+	Renderer ren;
+	ren.Initialize();
+	while (true)
 	{
-	public:
-		Foo() = default;
-		Foo(int v) : x(v) {}
-		~Foo() = default;
-		int x;
-	};
-
-	LinkedList<Foo> ll;
-
-	auto prFn = [&]()
-		{
-			for (int i = 0; i < ll.Size(); ++i)
-			{
-				std::cout << std::setw(2) << ll[i].x << " ";
-			}
-			std::cout << std::endl;
-		};
-
-	ll.Add(Foo(0));
-	prFn();
-	ll.Add(Foo(1));
-	prFn();
-	ll.Add(Foo(2));
-	prFn();
-	ll.Add(Foo(3));
-	prFn();
-	ll.Add(Foo(4));
-	prFn();
-	ll.Remove(2);
-	prFn();
-	ll.Add(3);
-	prFn();
-	ll.Remove(2);
-	prFn();
-	ll.Remove(3);
-	prFn();
-	ll.Add(Foo(4));
-	prFn();
-	ll.Remove(0);
-	prFn();
-	ll.Add(Foo(8));
-	prFn();
-
-	//Renderer ren;
-	//ren.Initialize();
-	//while (true)
-	//{
-	//	ren.Render();
-	//}
+		ren.Render();
+	}
 
 	return 0;
 }
