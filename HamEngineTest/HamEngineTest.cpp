106c499d6fbf331abdd0a2f15f60aa66e84cc981
SIYEONG-PC\siyeong <sy1211w@gmail.com>
2025-03-20 17:44:20 +0900

    UnitTest setup

---
+++ HamEngineTest/HamEngineTest.cpp
@@ -0,0 +1,28 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include <random>
+
+import Math;
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace ham;
+
+namespace HamEngineTest
+{
+	TEST_CLASS(HamEngineTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			std::random_device rd;
+			// Constructor
+			{
+				Vec2 v0;
+				Assert::AreSame(v0.X, 0.f);
+				Assert::AreSame(v0.Y, 0.f);
+				
+			}
+		}
+	};
+}
